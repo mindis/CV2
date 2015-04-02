5bf869c410e824d118e62c82755c19aa9395855c
Luka Stout <lukastout@gmail.com>
2015-04-02 17:08:50 +0200

    New matching

--- lab/stitch.py
+++ lab/stitch.py
@@ -1,89 +1,82 @@
-import cv2
-import numpy as np
-import itertools
 import sys
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
 
-def findKeyPoints(img, template, distance=200):
-    detector = cv2.FeatureDetector_create("SIFT")
-    descriptor = cv2.DescriptorExtractor_create("SIFT")
+def drawMatches(img1, kp1, img2, kp2, matches):
+    """
+    img1,img2 - Grayscale images
+    kp1,kp2 - Detected list of keypoints through any of the OpenCV keypoint 
+              detection algorithms
+    matches - A list of matches of corresponding keypoints through any
+              OpenCV keypoint matching algorithm
+    """
 
-    skp = detector.detect(img)
-    skp, sd = descriptor.compute(img, skp)
+    # Create a new output image that concatenates the two images together
+    # (a.k.a) a montage
+    rows1 = img1.shape[0]
+    cols1 = img1.shape[1]
+    rows2 = img2.shape[0]
+    cols2 = img2.shape[1]
 
-    tkp = detector.detect(template)
-    tkp, td = descriptor.compute(template, tkp)
-
-    flann_params = dict(algorithm=1, trees=4)
-    flann = cv2.flann_Index(sd, flann_params)
-    idx, dist = flann.knnSearch(td, 1, params={})
-    del flann
-
-    dist = dist[:,0]/2500.0
-    dist = dist.reshape(-1,).tolist()
-    idx = idx.reshape(-1).tolist()
-    indices = range(len(dist))
-    indices.sort(key=lambda i: dist[i])
-    dist = [dist[i] for i in indices]
-    idx = [idx[i] for i in indices]
-    skp_final = []
-    for i, dis in itertools.izip(idx, dist):
-        if dis < distance:
-            skp_final.append(skp[i])
-
-    flann = cv2.flann_Index(td, flann_params)
-    idx, dist = flann.knnSearch(sd, 1, params={})
-    del flann
-
-    dist = dist[:,0]/2500.0
-    dist = dist.reshape(-1,).tolist()
-    idx = idx.reshape(-1).tolist()
-    indices = range(len(dist))
-    indices.sort(key=lambda i: dist[i])
-    dist = [dist[i] for i in indices]
-    idx = [idx[i] for i in indices]
-    tkp_final = []
-    for i, dis in itertools.izip(idx, dist):
-        if dis < distance:
-            tkp_final.append(tkp[i])
-
-    return skp_final, tkp_final
-
-def drawKeyPoints(img, template, skp, tkp, num=-1):
-    h1, w1 = img.shape[:2]
-    h2, w2 = template.shape[:2]
-    nWidth = w1+w2
-    nHeight = max(h1, h2)
-    hdif = (h1-h2)/2
-    newimg = np.zeros((nHeight, nWidth, 3), np.uint8)
-    newimg[hdif:hdif+h2, :w2] = template
-    newimg[:h1, w2:w1+w2] = img
-
-    maxlen = min(len(skp), len(tkp))
-    if num < 0 or num > maxlen:
-        num = maxlen
-    for i in range(num):
-        pt_a = (int(tkp[i].pt[0]), int(tkp[i].pt[1]+hdif))
-        pt_b = (int(skp[i].pt[0]+w2), int(skp[i].pt[1]))
-        cv2.line(newimg, pt_a, pt_b, (255, 0, 0))
-    return newimg
-
-
-def match():
-    img = cv2.imread(sys.argv[1])
-    temp = cv2.imread(sys.argv[2])
-    try:
-    	dist = int(sys.argv[3])
-    except IndexError:
-    	dist = 200
-    try:
-    	num = int(sys.argv[4])
-    except IndexError:
-    	num = -1
-    skp, tkp = findKeyPoints(img, temp, dist)
-    newimg = drawKeyPoints(img, temp, skp, tkp, num)
-    cv2.imshow("image", newimg)
-    cv2.waitKey(0)
-
-    return skp, tkp
+    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
     
-keypoints_1, keypoints_2 = match()
+    # Place the first image to the left
+    out[:rows1,:cols1,:] = img1
+    # Place the next image to the right of it
+    out[:rows2,cols1:cols1+cols2,:] = img2
+
+    # For each pair of points we have between both images
+    # draw circles, then connect a line between them
+    for mat in matches:
+
+        # Get the matching keypoints for each of the images
+        img1_idx = mat.queryIdx
+        img2_idx = mat.trainIdx
+
+        # x - columns
+        # y - rows
+        (x1,y1) = kp1[img1_idx].pt
+        (x2,y2) = kp2[img2_idx].pt
+
+        # Draw a small circle at both co-ordinates
+        # radius 4
+        # colour blue
+        # thickness = 1
+        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)   
+        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+
+        # Draw a line in between the two points
+        # thickness = 1
+        # colour blue
+        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+
+
+    # Show the image
+    cv2.imshow('Matched Features', out)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+img1 = cv2.imread(sys.argv[1])        # queryImage
+img2 = cv2.imread(sys.argv[2])        # trainImage
+
+# Initiate SIFT detector
+sift = cv2.SIFT()
+
+# find the keypoints and descriptors with SIFT
+kp1, des1 = sift.detectAndCompute(img1,None)
+kp2, des2 = sift.detectAndCompute(img2,None)
+
+# create BFMatcher object
+bf = cv2.BFMatcher()
+
+# Match descriptors.
+matches = bf.match(des1,des2)
+
+# Sort them in the order of their distance.
+matches = sorted(matches, key = lambda x:x.distance)
+
+# Draw first 10 matches.
+drawMatches(img1,kp1,img2,kp2,matches[:10])
+
