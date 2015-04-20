0f72e02bee13801f2cbdbf8efc20b0af03550c18
Luka Stout <lukastout@gmail.com>
2015-04-20 18:36:52 +0200

    Made ransac generic

--- lab/fundamental_matrix.py
+++ lab/fundamental_matrix.py
@@ -24,29 +24,18 @@ def normalized_t(points):
     T.append([0, square_2 / mean_distance, -mean_y * square_2 / mean_distance])
     T.append([0, 0, 1])
     return T
+  
 
-def eight_point_algorithm(points1, points2, n_sample=8, normalised=True):
-    n_sample = min(n_sample, len(points1))
-
-    if normalised:
-        T_1 = normalized_t(points1)
-        T_2 = normalized_t(points2)
-    
+def eight_point(points, **kwargs):    
     A = []
     
-    for points in random.sample(zip(points1, points2), n_sample):
-        (x1, y1), (x2, y2) = points
-        if normalised:
-            x1, y1, _ = np.dot(T_1, [x1, y1, 1])
-            x2, y2, _ = np.dot(T_2, [x2, y2, 1])
+    for sample in points:
+        (x1, y1), (x2, y2) = sample
         A.append([x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1])
 
     U, s, V = np.linalg.svd(A)
     smallest_s_index =  np.argmin(s)
     F = np.array(V[smallest_s_index]).reshape((3, 3))
-
-    if normalised:
-        return np.dot(T_2, np.dot(F, T_1))
 
     U, s, V = np.linalg.svd(F)
     smallest_s_index = np.argmin(s)
@@ -55,30 +44,53 @@ def eight_point_algorithm(points1, points2, n_sample=8, normalised=True):
     F = np.dot(U, np.dot(np.diag(s), V))
     return F / F[2, 2]
 
-def sampson_distance(coords, coords2, F):
-    num = np.dot(np.array(coords2).T, np.dot(F, np.array(coords))) ** 2
-    Fp1 = np.dot(F, coords)
+def normalized_eight_point(points, T1, T2):
+    A = []
+    for sample in points:
+        (x1, y1), (x2, y2) = sample
+        x1, y1, _ = np.dot(T1, [x1, y1, 1])
+        x2, y2, _ = np.dot(T2, [x2, y2, 1])
+        A.append([x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1])
+
+    U, s, V = np.linalg.svd(A)
+    smallest_s_index =  np.argmin(s)
+    F = np.array(V[smallest_s_index]).reshape((3, 3))
+
+    return np.dot(T2, np.dot(F, T1))  
+
+def sampson_distance(points, F):
+    coords1, coords2 = points
+    coords1 = np.append(coords1, 1)
+    coords2 = np.append(coords2, 1)
+
+    num = np.dot(np.array(coords2).T, np.dot(F, np.array(coords1))) ** 2
+    Fp1 = np.dot(F, coords1)
     Fp2 = np.dot(F, coords2)
     denum = Fp1[0] ** 2 + Fp1[1] ** 2 + Fp2[0] ** 2 + Fp2[1] ** 2
     return num / denum
 
-def ransac(points1, points2, max_iter=50):
+def ransac(sample_pop, n_iter=50, algorithm=eight_point,
+        error_function=sampson_distance, n_samples=8,
+        additional_args=None):
+    
     smallest_err = float('inf')
-    best_F = None
-    for _ in xrange(max_iter):
+    best = None
+    n_samples = min(n_samples, len(points1))
 
-        F = eight_point_algorithm(points1, points2)
+    for _ in xrange(n_iter):
+        sample = random.sample(zip(points1, points2), n_samples)
+        result = algorithm(sample, **additional_args)
         err = 0
-        for point in zip(points1, points2):
-            coords1 = np.append(point[0], 1)
-            coords2 = np.append(point[1], 1)
-            err += sampson_distance(coords1, coords2, F)
+        for points in zip(points1, points2):
+            err += error_function(points, result)
 
         if err < smallest_err:
             smallest_err = err
-            best_F = F
+            best = result
     
-    return best_F
+    return best
+
+
 
 def blur_image(img, gaussian_size=(3,3)):
     return cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), gaussian_size, 0)
@@ -122,8 +134,20 @@ if __name__ == "__main__":
     points1 = np.array(points1)
     points2 = np.array(points2)
 
+    sample_pop = zip(points1, points2)
+    f = eight_point
+    normalize = True
+    additional_args = {}
+    if normalize:
+        T1 = normalized_t(points1)
+        T2 = normalized_t(points2)
+        additional_args['T1'] = T1
+        additional_args['T2'] = T2
+        f = normalized_eight_point
 
-    F = ransac(points1, points2, max_iter=1000)
+    F = ransac(sample_pop, n_iter=1000, algorithm=f,
+            error_function=sampson_distance, 
+            additional_args=additional_args)
     print F
 
     #Draw epipolar lines
