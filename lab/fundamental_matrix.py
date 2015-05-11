2787b4fe86500d0e64c82c5a1b64f4d526dfca59
finde <finde.xumara@gmail.com>
2015-05-11 14:13:20 +0200

    point-view matrix

--- lab/fundamental_matrix.py
+++ lab/fundamental_matrix.py
@@ -6,6 +6,9 @@ import numpy as np
 from ransac import ransac
 from scipy.spatial import distance
 from matplotlib import pyplot as plt
+from os import listdir
+from os.path import isfile, join
+import pandas as pd
 
 
 def filter_matches(matches, ratio=0.75):
@@ -36,13 +39,12 @@ def eight_point(points, **kwargs):
         A.append([xp * x, xp * y, xp, yp * x, yp * y, yp, x, y, 1])
 
     U, s, V = np.linalg.svd(A)
+    F = V[-1].reshape(3, 3)
 
-    F = V[-1].reshape(3,3)
-    
     U, s, V = np.linalg.svd(F)
     s[2] = 0
     F = np.dot(U, np.dot(np.diag(s), V))
-    
+
     return F / F[2, 2]
 
 
@@ -56,14 +58,15 @@ def normalized_eight_point(points, T1, T2):
         A.append([x1 * x2, x1 * y2, x1, y1 * x2, y1 * y2, y1, x2, y2, 1])
 
     U, s, V = np.linalg.svd(A)
-    smallest_s_index = np.argmin(s)
-    F = np.array(V[smallest_s_index]).reshape((3, 3))
+    F = np.array(V[-1]).reshape((3, 3))
+
     U, s, V = np.linalg.svd(F)
     smallest_s_index = np.argmin(s)
     s[smallest_s_index] = 0
-
     F = np.dot(U, np.dot(np.diag(s), V))
-    F = np.dot(T2, np.dot(F, T1))
+
+    F = np.dot(np.array(T1).T, np.dot(F, np.array(T2)))
+
     return F / F[2, 2]
 
 
@@ -82,33 +85,23 @@ def sampson_distance(points, F):
 def blur_image(img, gaussian_size=(3, 3)):
     return cv2.GaussianBlur(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY), gaussian_size, 0)
 
+
 def plot_epipolar_line(im, F, x):
     """ Plot the epipole and epipolar line F*x=0
         in an image. F is the fundamental matrix 
         and x a point in the other image."""
-    
-    m,n = im.shape[0], im.shape[1]
-    line = np.dot(F,x)
-    
+
+    m, n = im.shape[0], im.shape[1]
+    line = np.dot(F, x)
+
     # epipolar line parameter and values
-    t = np.linspace(0,n,100)
-    lt = np.array([(line[2]+line[0]*tt)/(-line[1]) for tt in t])
-    
-    plt.plot(t,lt,linewidth=2)
+    t = np.linspace(0, n, 100)
+    lt = np.array([(line[2] + line[0] * tt) / (-line[1]) for tt in t])
+
+    plt.plot(t, lt, linewidth=2)
 
 
-if __name__ == "__main__":
-    print
-    print 'Calculating epipolar lines', sys.argv[1], 'and', sys.argv[2]
-    print '=' * 80
-
-    save_as = None
-    if len(sys.argv) > 3:
-        save_as = sys.argv[3]
-
-    img1 = cv2.imread(sys.argv[1])[:, :, ::-1]
-    img2 = cv2.imread(sys.argv[2])[:, :, ::-1]
-
+def get_fundamental_matrix_for_pair_image(img1, img2, filter_match=0.75, n_iter=500, acceptance=0.25):
     # added blur to reduce noise
     img1_ = blur_image(img1)
     img2_ = blur_image(img2)
@@ -124,7 +117,7 @@ if __name__ == "__main__":
     matches = matcher.knnMatch(des1, trainDescriptors=des2, k=2)
     print "Match Count:\t\t", len(matches)
 
-    matches = filter_matches(matches, 0.75)
+    matches = filter_matches(matches, filter_match)
     print "Filtered Match Count:\t", len(matches)
 
     points1, points2 = [], []
@@ -138,9 +131,9 @@ if __name__ == "__main__":
 
     sample_pop = zip(points1, points2)
     f = eight_point
-    normalize = False
+    normalize = True
     additional_args = {}
-    
+
     if normalize:
         T1 = normalized_t(points1)
         T2 = normalized_t(points2)
@@ -148,13 +141,16 @@ if __name__ == "__main__":
         additional_args['T2'] = T2
         f = normalized_eight_point
 
-
     F, best_sample = ransac(sample_pop, algorithm=f,
-               error_function=sampson_distance,
-               additional_args=additional_args)
-    print F
+                            error_function=sampson_distance,
+                            additional_args=additional_args,
+                            acceptance=acceptance,
+                            n_iter=n_iter)
 
-    # TODO: Draw epipolar lines
+    return F, best_sample
+
+
+def draw_epipolar_line(img1, img2, F, best_sample):
     f, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
     ax1.imshow(img1)
     ax1.set_xlim([img1.shape[1], 0])
@@ -166,14 +162,74 @@ if __name__ == "__main__":
 
     for (point1, point2) in best_sample:
         plt.sca(ax1)
-        point = [point2[0], point2[1], 1]
+        point = [point1[0], point1[1], 1]
         plt.plot(point1[0], point1[1], '*')
-        plot_epipolar_line(img1, F, point)
+        plot_epipolar_line(img1, F.T, point)
 
         plt.sca(ax2)
-        point = [point1[0], point1[1], 1]
+        point = [point2[0], point2[1], 1]
         plt.plot(point2[0], point2[1], '*')
-        plot_epipolar_line(img2, F.T, point)
+        plot_epipolar_line(img2, F, point)
 
     plt.show()
+
+
+def generate_point_view_matrix(dirname):
+    # for all images in directory, create a sequential pair
+    point_view_matrix = pd.DataFrame()
+
+    images = [f for f in listdir(dirname) if isfile(join(dirname, f))]
+    images.append(images[0])
+
+    for i in xrange(len(images) - 1):
+        print
+        print 'Calculating epipolar lines', images[i], 'and', images[i + 1]
+        print '=' * 80
+
+        img1 = cv2.imread(dirname + '/' + images[i])[:, :, ::-1]
+        img2 = cv2.imread(dirname + '/' + images[i + 1])[:, :, ::-1]
+
+        F, inliers = get_fundamental_matrix_for_pair_image(img1, img2, n_iter=50, acceptance=0.01)
+
+        if point_view_matrix.shape[1] == 0:
+            for iix, (point1, point2) in enumerate(inliers):
+                fp = 'fp%d' % (iix + 1)
+                # point_view_matrix[fp] = []
+                point_view_matrix.ix['img%d' % i, fp] = '%f_%f' % (point1[0], point1[1])
+                point_view_matrix.ix['img%d' % (i + 1), fp] = '%f_%f' % (point2[0], point2[1])
+        else:
+            for (point1, point2) in inliers:
+                # if the point1 exists in the previous img
+                if '%f_%f' % (point1[0], point1[1]) in point_view_matrix.loc['img%d' % i]:
+                    col = point_view_matrix.columns[point_view_matrix.ix['img2'] == '%f_%f' % (point1[0], point1[1])][0]
+                    point_view_matrix.ix['img%d' % (i + 1), col] = '%f_%f' % (point2[0], point2[1])
+                else:
+                    fp = 'fp%d' % (iix + point_view_matrix.shape[1])
+                    point_view_matrix.ix['img%d' % i, fp] = '%f_%f' % (point1[0], point1[1])
+                    point_view_matrix.ix['img%d' % (i + 1), fp] = '%f_%f' % (point2[0], point2[1])
+
+                # todo: add to point_view_matrix, where row = view_point, and columns = feature_point
+
+    # binarization
+    point_view_matrix = point_view_matrix.clip(upper=1)
+    point_view_matrix = point_view_matrix.fillna(0)
+
+    return point_view_matrix
+
+
+if __name__ == "__main__":
+    PVM = generate_point_view_matrix(sys.argv[1])
+
+    plt.imshow(PVM.as_matrix(), interpolation='nearest', cmap='Greys')
+    plt.show()
+    # # Draw epipolar lines
+    # draw_epipolar_line(img1, img2, F, inliers[:8])
+    #
+    # print F
+    #
+    # # inliers are the match between two images
+    # print inliers
+    # Chaining
+    # todo: convert points to 3d space
+    # todo: create point-view matrix
 
--- lab/ransac.py
+++ lab/ransac.py
@@ -1,25 +1,62 @@
 import random
+import numpy as np
 
-def ransac(sample_pop, algorithm, error_function, n_samples=8, n_iter=100,
-        additional_args=None):
-    
-    smallest_err = float('inf')
-    best = None
+
+def get_error(error_function, model, test_points):
+    test_err = np.array([])
+    for points in test_points:
+        test_err = np.append(test_err, error_function(points, model))
+
+    return test_err
+
+
+# lo-ransac
+def ransac(sample_pop, algorithm, error_function, n_samples=8, n_iter=5000,
+           t=1E-6, acceptance=0.25, verbose=False,
+           additional_args=None):
+    # modified to reflect http://en.wikipedia.org/w/index.php?title=RANSAC&oldid=116358182
+
+    i = 0
+    d = acceptance * len(sample_pop)
+    sample_pop = np.array(sample_pop)
+    bestfit = None
+    best_inlier_idxs = []
+    besterr = np.inf
     if n_samples > len(sample_pop):
         raise Exception('''It is impossible to sample %d samples from 
                 population with length %d''' % (n_samples, len(sample_pop)))
 
-    for _ in xrange(n_iter):
-        sample = random.sample(sample_pop, n_samples)
-        result = algorithm(sample, **additional_args)
-        err = 0
-        for points in sample_pop:
-            err += error_function(points, result)
+    while i < n_iter:
+        maybe_idxs, test_idxs = random_partition(n_samples, len(sample_pop))
+        maybeinliers = sample_pop[maybe_idxs]
+        test_points = sample_pop[test_idxs]
 
-        if err < smallest_err:
-            smallest_err = err
-            best = result
-            best_sample = sample
-    
-    return best, best_sample
+        maybemodel = algorithm(maybeinliers, **additional_args)
+        test_err = get_error(error_function, maybemodel, test_points)
+        also_idxs = test_idxs[test_err < t]  # select indices of rows with accepted points
+        alsoinliers = sample_pop[also_idxs]
 
+        if len(alsoinliers) > d:
+            betterdata = np.concatenate((maybeinliers, alsoinliers))
+            bettermodel = algorithm(betterdata, **additional_args)
+            better_errs = get_error(error_function, bettermodel, betterdata)
+            thiserr = np.mean(better_errs)
+            if thiserr < besterr:
+                bestfit = bettermodel
+                besterr = thiserr
+                best_inlier_idxs = np.concatenate((maybe_idxs, also_idxs))
+
+            if verbose:
+                print 'iteration %d - best inlier %d' % (i, len(best_inlier_idxs))
+        i += 1
+
+    return bestfit, sample_pop[best_inlier_idxs]
+
+
+def random_partition(n, n_data):
+    """return n random rows of data (and also the other len(data)-n rows)"""
+    all_idxs = np.arange(n_data)
+    np.random.shuffle(all_idxs)
+    idxs1 = all_idxs[:n]
+    idxs2 = all_idxs[n:]
+    return idxs1, idxs2
